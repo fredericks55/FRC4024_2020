ea5cda0e7f6cd6324be846694782aea8388f2175
Ben Pringle <benjamin.m.pringle@gmail.com>
2021-03-29 08:32:26 -0400

    this commit adds a debug mode to debug hardware problems and documents test code better for 2021.

--- TestBot/src/main/java/frc/robot/Robot.java
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -13,7 +13,11 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+import static edu.wpi.first.wpilibj.GenericHID.Hand.*;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.time.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -24,15 +28,37 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 public class Robot extends TimedRobot {
 
   //the four TalonSRXs that control the wheels
-  public TalonSRX rightFront;
-  public TalonSRX rightBack;
-  public TalonSRX leftFront;
-  public TalonSRX leftBack;
+  public TalonSRX right1;
+  public TalonSRX right2;
+  public TalonSRX left1;
+  public TalonSRX left2;
+  public TalonSRX ballGear;
 
+  //ultrasonic sensor
+  public AnalogInput ultrasonicSensor;
+
+  //flag to check if we should turn the ball gear
+  public boolean isBallGearOn = false;
+
+  //timer
   public Timer timer;
+
+  //controller
   public XboxController controller;
 
+  //double solenoid piston
   public DoubleSolenoid piston1;
+
+  //percent of max speed to run at
+  public double aspeed = 0.3;
+
+  public boolean debugMode = true;
+
+  //should move
+  public boolean mov = false;
+
+  //debug instant for timing things
+  Instant start;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -43,17 +69,25 @@ public class Robot extends TimedRobot {
     System.out.println("robot init");
 
     //init talon
-    rightFront = new TalonSRX(1); //port 1 on CAN
-    rightBack = new TalonSRX(2); //port 2 on CAN
-    leftFront = new TalonSRX(3); //port 3 on CAN
-    leftBack = new TalonSRX(4); //port 4 on CAN*/
+    right1 = new TalonSRX(5); //port 5 on CAN
+    right2 = new TalonSRX(4); //port 4 on CAN
+    left1 = new TalonSRX(3); //port 3 on CAN
+    left2 = new TalonSRX(1); //port 1 on CAN*/
+
+    ballGear = new TalonSRX(2); //port 2 on CAN
+
+    ultrasonicSensor = new AnalogInput(0); // port () on CAN
 
     controller = new XboxController(0); //port 0 on driver station
 
-    piston1 = new DoubleSolenoid(0, 1); //i really have no idea how this works. 
-    piston1.set(kForward);
+    /* comment out becasue we don't have a piston attached rn
+      piston1 = new DoubleSolenoid(0, 1); //set ports used to be 0 and 1 on the pcm 
+      piston1.set(kForward); //set it to start in the forwards position
+    */
 
     timer = new Timer(); // init timer
+
+    start = Instant.now(); //init instant with current time
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -78,19 +112,80 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    System.out.println("teleop periodic");
-    
-    if(controller.getAButtonPressed()) {
-      piston1.toggle();
-    }
+    if(debugMode) {
+      System.out.println("teleop periodic");
+      long timeElapsed = Duration.between(start, Instant.now()).toMillis();
+  
+      if(controller.getAButtonPressed()) {
+        aspeed += 0.02;
+      } 
+      if(controller.getBButtonPressed()) {
+        aspeed -= 0.02;
+      } 
+      if(controller.getYButtonPressed()) {
+        start = Instant.now();
+      }
+      if(controller.getXButtonPressed()) {
+        mov = !mov;
+      }
+      System.out.println(aspeed);
+      System.out.println(mov);
+      if(timeElapsed >= 1000) {
+        right1.set(ControlMode.PercentOutput, 0);
+        right2.set(ControlMode.PercentOutput, 0);
+        left1.set(ControlMode.PercentOutput, 0);
+        left2.set(ControlMode.PercentOutput, 0);
+      }else if(mov) {
+        right1.set(ControlMode.PercentOutput, -aspeed);
+        right2.set(ControlMode.PercentOutput, -aspeed);
+        left1.set(ControlMode.PercentOutput, aspeed);
+        left2.set(ControlMode.PercentOutput, aspeed);
+      }
+      SmartDashboard.putNumber("DB/Slider 0", aspeed);
+    }else {
+      if(controller.getAButtonPressed()) {
+        //piston1.toggle();
+      }
 
-    //y is negated because AFAIK joystick up is negative for some unknowable reason (thanks WPILIB, appreciate it)
-    /*double speed = Math.min(-controller.getY(), 0.9); //dont go faster than 90%
-    rightFront.set(ControlMode.PercentOutput, speed);
-    leftFront.set(ControlMode.PercentOutput, speed);
-    rightBack.set(ControlMode.PercentOutput, speed);
-    leftBack.set(ControlMode.PercentOutput, speed);
-    System.out.println("speed: " + controller.getY());*/
+      if(controller.getBButtonPressed()) {
+        isBallGearOn = !isBallGearOn;
+      }
+
+      if(isBallGearOn) {
+        ballGear.set(ControlMode.PercentOutput, 0.4);
+      }else {
+        ballGear.set(ControlMode.PercentOutput, 0);
+      }
+
+      if(controller.getBumper(kLeft)) {
+        double speed = 0.2;
+        right1.set(ControlMode.PercentOutput, -speed);
+        right2.set(ControlMode.PercentOutput, -speed);
+        left1.set(ControlMode.PercentOutput, -speed);
+        left2.set(ControlMode.PercentOutput, -speed);
+      }else if(controller.getBumper(kRight)) {
+        double speed = 0.2;
+        right1.set(ControlMode.PercentOutput, speed);
+        right2.set(ControlMode.PercentOutput, speed);
+        left1.set(ControlMode.PercentOutput, speed);
+        left2.set(ControlMode.PercentOutput, speed);
+      }else {
+        //y is negated because AFAIK joystick up is negative for some unknowable reason (thanks WPILIB, appreciate it)
+        double speed = Math.min(-controller.getY(kLeft) * 0.2 , 0.2); //dont go faster than 90%
+        right1.set(ControlMode.PercentOutput, -speed);
+        right2.set(ControlMode.PercentOutput, -speed);
+        left1.set(ControlMode.PercentOutput, speed);
+        left2.set(ControlMode.PercentOutput, speed);
+        System.out.println("speed: " + controller.getY(kLeft));
+      }
+
+      if(controller.getYButton()) {
+        right1.set(ControlMode.PercentOutput, 0);
+        right2.set(ControlMode.PercentOutput, 0);
+        left1.set(ControlMode.PercentOutput, 0);
+        left2.set(ControlMode.PercentOutput, 0);
+      }
+    }
   }
 
   /** This function is called once each time the robot enters test mode. */
